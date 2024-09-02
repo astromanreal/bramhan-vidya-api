8ebbf3c4364b706bb74d9c91c07ed7957f1412ba
astromanreal <astroman@gmail.com>
2024-09-02 11:21:36 +0530

    updating files

--- src/models/blogs/blogModel.js
+++ src/models/blogs/blogModel.js
@@ -1,49 +1,52 @@
 const mongoose = require("mongoose");
 const Connections = require("../../config/db");
 
-const blogSchema = new mongoose.Schema({
-  title: {
-    type: String,
-    required: true,
-  },
-  subtitle: {
-    type: String,
-  },
-  views: { type: Number, default: 1 },
-  userId: { type: mongoose.Schema.Types.ObjectId, required: true },
-  keyDetails: [
-    {
-      key: String,
-      value: String,
+const blogSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
     },
-  ],
-  content: [
-    {
-      key: String,
-      value: String,
-    },
-  ],
-  category: {
-    type: String,
-    enum: [
-      "Scriptures",
-      "Deities",
-      "Festivals",
-      "Events",
-      "Places",
-      "Philosophy",
-      "Other",
-    ],
-  },
-
-  tags: [
-    {
+    subtitle: {
       type: String,
     },
-  ],
-  image: {
-    type: String,
+    views: { type: Number, default: 1 },
+    userId: { type: mongoose.Schema.Types.ObjectId, required: true },
+    keyDetails: [
+      {
+        key: String,
+        value: String,
+      },
+    ],
+    content: [
+      {
+        key: String,
+        value: String,
+      },
+    ],
+    category: {
+      type: String,
+      enum: [
+        "Scriptures",
+        "Deities",
+        "Festivals",
+        "Events",
+        "Places",
+        "Philosophy",
+        "Other",
+      ],
+    },
+
+    tags: [
+      {
+        type: String,
+      },
+    ],
+    image: {
+      type: String,
+    },
   },
-});
+  { timestamps: true }
+);
 
 module.exports = Connections.blogConnection.model("Blog", blogSchema);
